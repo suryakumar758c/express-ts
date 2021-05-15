ffd020ee4820e4a1f79764cd74484c59ff1a8de9
suryakumar758c <suryakumar.c@optisolbusiness.com>
2021-05-15 17:27:16 +0530

    [updates] mysql and docker compose file updates

--- server/models/mysql/connection.ts
+++ server/models/mysql/connection.ts
@@ -1,14 +1,20 @@
-const { MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE } =
-  process.env;
+const {
+  MYSQL_HOST,
+  MYSQL_PORT = 3306,
+  MYSQL_USER,
+  MYSQL_ROOT_PASSWORD,
+  MYSQL_DB,
+} = process.env;
 
 import { Sequelize } from "sequelize";
 
 const connection: Sequelize = new Sequelize({
   dialect: "mysql",
   host: MYSQL_HOST,
-  username: MYSQL_USERNAME,
-  password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE,
+  port: Number(MYSQL_PORT),
+  username: MYSQL_USER,
+  password: MYSQL_ROOT_PASSWORD,
+  database: MYSQL_DB,
 });
 
 connection
